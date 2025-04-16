bf5f89b9c016ab3dac082885292576df66f3a48a
Dimitrie Lauer <112709753+ItsDimitrie@users.noreply.github.com>
2025-04-16 10:05:30 +0200

    Revert "Changed script to widget embed."
    This reverts commit 6e5288eba486234328c53b33bb94ae04e7dbfd5e.

--- dist/embed.js
+++ dist/embed.js
@@ -1,40 +1,43 @@
 (function () {
-  const divContainer = document.querySelector(".widget-data");
+    const divContainer = document.querySelector('.widget-data');
 
-  const config = {
-    dataRestaurant: divContainer
-      ? divContainer.getAttribute("restoId") || ""
-      : "",
-    color: divContainer ? divContainer.getAttribute("color") || "" : "",
-  };
+    const config = {
+        dataRestaurant: divContainer ? divContainer.getAttribute('restoId') || '' : '',
+        color: divContainer ? divContainer.getAttribute('color') || '' : '',
+    };
 
-  let iframe = document.getElementById("reservation-widget-iframe");
+    let iframe = document.getElementById("reservation-widget-iframe");
 
-  if (!iframe) {
-    restaurantId = config.dataRestaurant;
-    color = config.color;
-    console.log(restaurantId);
+    if (!iframe) {
 
-    iframe = document.createElement("iframe");
-    iframe.src = ` https://itsdimitrie.github.io/table.one-widget-test?restaurantId=${restaurantId}&color=${color}`;
-    iframe.id = "reservation-widget-iframe";
-    iframe.style.all = "initial";
-    iframe.style.position = "fixed";
-    iframe.style.bottom = "1px";
-    iframe.style.right = "1px";
+        restaurantId = config.dataRestaurant
+        color = config.color
+        console.log(restaurantId)
 
-    if (divContainer) {
-      divContainer.appendChild(iframe);
-    } else {
-      document.body.appendChild(iframe);
+        iframe = document.createElement("iframe");
+        iframe.src = ` https://itsdimitrie.github.io/table.one-widget-test?restaurantId=${restaurantId}&color=${color}`;
+        // iframe.src = `http://localhost:5173/table.one-widget-test?restaurantId=${restaurantId}&color=${color}`;
+        iframe.id = "reservation-widget-iframe";
+        iframe.style.all = 'initial'
+        iframe.style.position = 'fixed'
+        iframe.style.bottom = '1px'
+        iframe.style.right = '1px'
+
+       
+
+        if (divContainer) {
+            divContainer.appendChild(iframe);
+        } else {
+            document.body.appendChild(iframe);
+        }
     }
-  }
 
-  // Listen for messages from the iframe (Vue app)
-  window.addEventListener("message", (event) => {
-    if (event.data.type === "resizeWidget") {
-      iframe.style.width = event.data.width;
-      iframe.style.height = event.data.height;
-    }
-  });
+    // Listen for messages from the iframe (Vue app)
+    window.addEventListener('message', (event) => {
+        if (event.data.type === 'resizeWidget') {
+            iframe.style.width = event.data.width;
+            iframe.style.height = event.data.height;
+        }
+    });
+
 })();
